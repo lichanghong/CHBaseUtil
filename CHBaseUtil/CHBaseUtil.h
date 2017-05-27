c7671d7f20bf8f4c64b784481ddd2f7fd64397d7
李长鸿 <lichanghong@b.360.cn>
2017-05-27 14:06:11 +0800

    0.0.1

---
+++ CHBaseUtil/CHBaseUtil.h
@@ -0,0 +1,19 @@
+//
+//  CHBaseUtil.h
+//  CHBaseUtil
+//
+//  Created by lichanghong on 5/27/17.
+//  Copyright © 2017 lichanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHBaseUtil.
+FOUNDATION_EXPORT double CHBaseUtilVersionNumber;
+
+//! Project version string for CHBaseUtil.
+FOUNDATION_EXPORT const unsigned char CHBaseUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHBaseUtil/PublicHeader.h>
+
+
